85f513bd24d572e6f35ef0457eb65f9cad540944
Alilln <broal@qip.ru>
2017-12-08 00:46:01 +0300

    update factory

--- js/main.js
+++ js/main.js
@@ -1,21 +1,31 @@
 function ready() {
+    'use strict';
 
     function BigCard(name, desc) {
         this.cardType = "big";
-        this.width = desc.width || 11.3;
-        this.height = desc.height || 220;
+        this.theme = "game__card--" + desc.theme;
+        this.size = " game__card--big";
+        this.class = desc.class || (this.theme + this.size);
+        this.el = `<div class='${this.class}'></div>`;
+        this.checked = false;
     }
 
     function MediumCard(name, desc) {
         this.cardType = "medium";
-        this.width = desc.width || 15;
-        this.height = desc.height || 220;
+        this.theme = desc.theme;
+        this.size = " game__card--medium";
+        this.class = desc.class || (this.theme + this.size);
+        this.el = `<div class='${this.class}'></div>`;
+        this.checked = false;
     }
 
     function SmallCard(name, desc) {
         this.cardType = "small";
-        this.width = desc.width || 18.5;
-        this.height = desc.height || 330;
+        this.theme = desc.theme;
+        this.size = " game__card--small";
+        this.class = desc.class || (this.theme + this.size);
+        this.el = `<div class='${this.class}'></div>`;
+        this.checked = false;
     }
 
     function CardFactory() { };
@@ -33,15 +43,16 @@ function ready() {
 
     let myCardFactory = new CardFactory();
 
-    let myCard = myCardFactory.makeCard("big", {
-        cardType: "big",
-        width: 11.3,
-        height: 230
-    });
+    let cardField = document.querySelector(".game__field");
+    let arrCards = [];
 
-    let myCard1 = myCardFactory.makeCard("medium", {});
-
-    console.log(myCard1);
+    for (let i = 0; i < 24; i++) {
+        let myCard = myCardFactory.makeCard("big", {
+            theme: 'madagascar'
+        });
+        cardField.insertAdjacentHTML("afterBegin", myCard.el);
+        arrCards.push(myCard);
+    }
 }
 
 document.addEventListener("DOMContentLoaded", ready);
