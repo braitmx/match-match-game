4c665237438b1c9b46e9ef2d904a17c682d694ee
Alilln <broal@qip.ru>
2017-12-09 22:17:41 +0300

    add invisible blocks

--- js/turnCards.js
+++ js/turnCards.js
@@ -21,9 +21,15 @@
             clicked: false
         },
         {
-            name: 'block3',
+            name: 'block4',
             el: "<div id='4' data-theme='green' class='block'></div>",
             addClass: "green",
+            clicked: false
+        },
+        {
+            name: 'block5',
+            el: "<div id='5' data-theme='red' class='block'></div>",
+            addClass: "red",
             clicked: false
         }
 
@@ -34,15 +40,16 @@
     function renderBlocks() {
         arrBlocks.forEach(function (item) {
             let el = item.el;
-            console.log('render');
+
             area.insertAdjacentHTML("beforeEnd", el);
         });
     };
 
     renderBlocks();
 
-    let elements = document.querySelectorAll('.block'),
-        counter = 0,
+    let elements = document.querySelectorAll('.block');
+
+    let counter = 0,
         prevCard;
 
     function toggleCard(e) {
@@ -76,10 +83,13 @@
                 props = [prevCard, thisCard];
 
             if (prevCard.dataset.theme === this.dataset.theme && prevCard.id != this.id) {
+                console.log('done!');
 
                 setTimeout(function () {
-                    area.removeChild(props[0]);
-                    area.removeChild(props[1]);
+
+                   props[0].classList.add("invisble");
+                   props[1].classList.add("invisble");
+
 
                 }.bind(props), 2000);
 
@@ -91,7 +101,6 @@
         }
 
         setTimeout(function () {
-
             that.addEventListener('click', toggleCard);
         }, 4000);
 
